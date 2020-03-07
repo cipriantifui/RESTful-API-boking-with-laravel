bdebb86941d2879b5483bc17074822fa5c3ba4cc
ciprian92 <cipriantifui92@gmail.com>
2020-03-07 10:07:41 +0200

    create UserApiRepositori and change FactoryInterface RepositoryInterface namespace

---
+++ app/Repositories/UserApiRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Repositories\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Interfaces\RepositoryInterface;
+
+class UserApiRepository implements RepositoryInterface
+{
+
+}
